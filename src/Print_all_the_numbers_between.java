db87557a553412eb6c8091f162944b64df31235d
Sami <sami@Samis-MacBook-Pro.local>
2021-08-30 22:28:37 +0530

    Print all the numbers between -15 to 10.

---
+++ src/Print_all_the_numbers_between.java
@@ -0,0 +1,15 @@
+
+public class Print_all_the_numbers_between {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1: Print all the numbers between -15 to 10.
+		
+		for(int i=-15;i<=10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
