ba0bda0f106c1a616b0ed01e647875c2b184feaa
Sami <sami@Samis-MacBook-Pro.local>
2021-08-30 22:38:46 +0530

    Ask user to enter a number and then print the multiplication table of the input number.

---
+++ src/Ask_user_to_enter_a_number_and_then_print_the_multiplication_table_of_the_input_number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ask_user_to_enter_a_number_and_then_print_the_multiplication_table_of_the_input_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter number for which u want to print table");
+		int number =s.nextInt();
+		
+		for(int i =1;i<=10;i++) {
+			
+			int product = number*i;
+			System.out.println(number+"*"+i+" = "+product);
+			
+		}
+		
+		
+		
+	}
+
+}
